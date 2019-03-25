329894659e9e6f0caeac85a640537e79232e08ac
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-25 15:04:07 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,7 +33,7 @@ def hit?(card_total)
   prompt_user
   user_input = get_user_input
   if user_input == "h"
-    deal_card
+    card_total += deal_card
   else if user_input != "h" && user_input != "s"
     invalid_command
   end
